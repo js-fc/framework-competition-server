b4d0b129d4fba3f80c120857a1afe24bbcca79d4
polyathlon <polyatlon.rsu.edu.ru@gmail.com>
2022-10-13 16:50:25 +0300

    Just Format

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,15 @@
 import * as express from 'express';
-const app = express();
+const app: express.Application = express();
 // routes = require('./routes/index')
 
 const host = '127.0.0.1'
 const port = 7000
 
-console.log(`Server listens http://${host}:${port}`)
-
-// app.use(express.json())
-// app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // app.use('/api', routes)
 
-// app.listen(port, host, () =>
-//   console.log(`Server listens http://${host}:${port}`)
-// )
+app.listen(port, host, () =>
+    console.log(`Server listens http://${host}:${port}`)
+)
